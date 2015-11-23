97b232f5384ae390d634ae7282d2e9ebba722114
sunnythakral <sunnythakral786@gmail.com>
2015-11-22 20:03:42 -0500

    FoodTracker swift App

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Sunny Thakral on 2015-10-26.
+//  Copyright © 2015 Sunny Thakral Corp. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
